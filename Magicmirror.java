6f4e5fea95022c9dc5fee2fae4f7b7687565da1b
Aman Mishra <23053103@kiit.ac.in>
2025-06-25 00:05:56 +0530

    Add files via upload

---
+++ Magicmirror.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Magicmirror {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            stack.push(ch);
+        }
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+        System.out.println("Reversed string:" + reversed.toString());
+    }
+}
